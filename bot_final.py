c7d7937890530b485f326d86f530b5c7e67c2bd1
Samriddhi Singh <simisinghch@gmail.com>
2021-10-14 17:49:40 +0200

    Update bot_final.py

--- bot_final.py
+++ bot_final.py
@@ -1,3 +1,4 @@
+#download chromedriver from https://chromedriver.chromium.org/
 import time
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
@@ -5,8 +6,8 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
 driver = webdriver.Chrome(executable_path="/path/to/chromedriver")
-contact = input('enter contact as saved on whatsapp')
-text = input('enter text')
+contact = ""#enter the contact as saved on whatsapp
+text = ""#enter the text you want to send them
 
 
 driver.get("https://web.whatsapp.com")
@@ -14,7 +15,7 @@ print("Scan QR Code, And then Enter")
 input()
 print("Logged In")
 
-#inp_xpath_search is the full xpath to the search section
+#inp_xpath_search is the xpath to the search section(of contacts and group chats)
 inp_xpath_search = "/html/body/div/div[1]/div[1]/div[3]/div/div[1]/div/label/div/div[2]"
 input_box = driver.find_element_by_xpath(inp_xpath_search)
 #enters the contacts name
@@ -28,9 +29,8 @@ selected_contact.click()
 time.sleep(2)
 
 #finds the text section
-inp_xpath = "/html/body/div/div[1]/div[1]/div[4]/div[1]/footer/div[1]/div[2]/div/div[1]/div/div[2]"
+inp_xpath = '//*[@id="main"]/footer/div[1]/div/div/div[2]/div[1]/div'
 input_box = driver.find_element_by_xpath(inp_xpath)
 time.sleep(2)
-
-input_box.send_keys(text + Keys.ENTER)#here you it writes and sends the text
-time.sleep(2)
+for i in range(30):#repeats it 30 times
+    input_box.send_keys(text + Keys.ENTER)#here you it writes and sends the text
